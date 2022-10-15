a9bbfc66f572dda28f6ed7ecab5adcce3474c396
Shah Jainil <shahjainil2406@gmail.com>
2022-10-15 23:22:03 +0530

    Added form to enter new Expense, demonstarte way to use multiple state handling using single useState call, Two way binding when form submitted

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css"
+
+const ExpenseForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    // Method for using one useState to manage multiple states
+    // const[userInput, setUserInput] = userState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+
+    // });
+
+    const titleChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+        // Method for using one useState to manage multiple states and if we have dependency on previous state
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState, enteredTitle: event.target.value
+        //     };
+        // });
+    };
+
+    const dateChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredDate(event.target.value);
+        // Method for using one useState to manage multiple states and if we have dependency on previous state
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState, enteredAmount: event.target.value
+        //     };
+        // });
+    };
+
+    const amountChangeHandler = (event) => {
+        console.log(event.target.value);
+        setEnteredAmount(event.target.value);
+        // Method for using one useState to manage multiple states and if we have dependency on previous state
+        // setUserInput((prevState) => {
+        //     return {
+        //         ...prevState, enteredDate: event.target.value
+        //     };
+        // });
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    return(
+        <form onSubmit={submitHandler}>
+            <div className="new-expense_controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type="number" value={enteredAmount} onChange={amountChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" value={enteredDate} min="2022-01-01" step="2025-12-31" onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    )
+}
+
+export default ExpenseForm; 
